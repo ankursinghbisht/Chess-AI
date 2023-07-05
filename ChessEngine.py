e540806852f3f701ede5327ad7904f5bea976d96
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-07-06 02:12:52 +0530

    added board declaration

--- ChessEngine.py
+++ ChessEngine.py
@@ -4,3 +4,29 @@ Responsible for :
 -determining valid moves at current state.
 -keep move log
 """
+
+import numpy as np
+
+
+class GameState:
+    def __init__(self):
+        """
+        board of 8x8 space, with each piece having 2 attributes,
+        -it's color ( b or w for black or white )
+        - type of piece ( K- king , Q-Queen , R- Rook, B- Bishop, N-Knight, or p-pawn)
+        &  "--" represents empty space
+        """
+        # Create an empty chess board
+        self.board = np.empty((8, 8), dtype=object)
+        self.board.fill('--')
+
+        # Place the chess pieces on the board
+        self.board[0] = ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR']  # Black back row
+        self.board[1] = ['bp'] * 8  # Black pawns
+        self.board[6] = ['wp'] * 8  # White pawns
+        self.board[7] = ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']  # White back row
+
+        self.whiteToMove = True
+        self.moveLog = []
+
+
