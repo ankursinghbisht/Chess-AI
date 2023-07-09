156b4b25b4a36a91d3c01e266c7dd807084ca7c7
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-07-09 16:37:35 +0530

    implemented piece movements

--- ChessMain.py
+++ ChessMain.py
@@ -25,6 +25,60 @@ def loadImages():
 
 
 # main driver function , handles inputs and graphics update
+"""
+def main():
+    screen = p.display.set_mode((WIDTH, HEIGHT))  # set up the screen
+    p.display.set_caption("Chess")
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    loadImages()
+
+    dragging = False  # Flag to track if a piece is being dragged
+    selected_piece = None  # Stores the selected piece being dragged
+    selected_square = ()  # Stores the initial position of the selected piece
+    playerClicks = []  # Keeps track of player clicks (2 Tuples: ex, (6, 4) -> (4, 4))
+
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                if not dragging:
+                    location = p.mouse.get_pos()  # x, y coordinate of mouse click
+                    col = location[0] // SQ_SIZE
+                    row = location[1] // SQ_SIZE
+                    selected_square = (row, col)
+                    selected_piece = gs.board[row, col]
+                    playerClicks.append(selected_square)
+                    dragging = True
+            elif e.type == p.MOUSEBUTTONUP:
+                if dragging:
+                    location = p.mouse.get_pos()  # x, y coordinate of mouse release
+                    col = location[0] // SQ_SIZE
+                    row = location[1] // SQ_SIZE
+                    if (row, col) != selected_square:
+                        playerClicks.append((row, col))
+                        move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                        gs.makeMove(move)
+                    selected_piece = None
+                    selected_square = ()
+                    playerClicks = []
+                    dragging = False
+
+            if dragging:
+                # Draw the piece being dragged at the current mouse position
+                mouse_pos = p.mouse.get_pos()
+                screen.blit(IMAGES[selected_piece], p.Rect(mouse_pos[0]- SQ_SIZE / 2, mouse_pos[1] - SQ_SIZE / 2, SQ_SIZE, SQ_SIZE))
+            else:
+                drawGameState(screen, gs)
+
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+"""
+
 
 def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))  # set up the screen
@@ -56,11 +110,18 @@ def main():
                 else:
                     sqSelected = (row, col)
                     playerClicks.append(sqSelected)
-
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
 
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
+
+
 
 
 # responsible for all  graphics with current game state
