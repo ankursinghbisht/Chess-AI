605359c247ff031b3e4340cef8bf2f47419a2383
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-07-09 22:41:50 +0530

    tried adding drag and drop

--- ChessMain.py
+++ ChessMain.py
@@ -11,7 +11,7 @@ p.init()
 WIDTH = HEIGHT = 512  # size of our board
 DIMENSION = 8  # Dimension of our chess board is 8x8
 SQ_SIZE = WIDTH // DIMENSION  # size of each square
-MAX_FPS = 15
+MAX_FPS = 30
 
 # initialize global directory of images .This will be called exactly once in the main
 IMAGES = {}
@@ -25,7 +25,7 @@ def loadImages():
 
 
 # main driver function , handles inputs and graphics update
-"""
+
 def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))  # set up the screen
     p.display.set_caption("Chess")
@@ -35,8 +35,8 @@ def main():
     loadImages()
 
     dragging = False  # Flag to track if a piece is being dragged
-    selected_piece = None  # Stores the selected piece being dragged
-    selected_square = ()  # Stores the initial position of the selected piece
+    selected_piece = '--'  # Stores the selected piece being dragged
+    sqSelected = ()  # Stores the initial position of the selected piece
     playerClicks = []  # Keeps track of player clicks (2 Tuples: ex, (6, 4) -> (4, 4))
 
     running = True
@@ -49,37 +49,69 @@ def main():
                     location = p.mouse.get_pos()  # x, y coordinate of mouse click
                     col = location[0] // SQ_SIZE
                     row = location[1] // SQ_SIZE
-                    selected_square = (row, col)
-                    selected_piece = gs.board[row, col]
-                    playerClicks.append(selected_square)
+                    if gs.board[row, col] != '--':
+                        sqSelected = (row, col)
+                        playerClicks.append(sqSelected)
+                        selected_piece = gs.board[row, col]
+
                     dragging = True
+                    drawGameState(screen, gs)
+
             elif e.type == p.MOUSEBUTTONUP:
                 if dragging:
                     location = p.mouse.get_pos()  # x, y coordinate of mouse release
                     col = location[0] // SQ_SIZE
                     row = location[1] // SQ_SIZE
-                    if (row, col) != selected_square:
-                        playerClicks.append((row, col))
+                    if sqSelected == (row, col):  # user clicked on same square twice, i.e. it needed to be unselected
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        sqSelected = (row, col)
+                        playerClicks.append(sqSelected)
                         move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                        print(move.getChessNotation())
                         gs.makeMove(move)
-                    selected_piece = None
-                    selected_square = ()
-                    playerClicks = []
+                        sqSelected = ()
+                        playerClicks = []
                     dragging = False
 
-            if dragging:
+            drawGameState(screen, gs)
+
+            if dragging and selected_piece != '--':
                 # Draw the piece being dragged at the current mouse position
                 mouse_pos = p.mouse.get_pos()
-                screen.blit(IMAGES[selected_piece], p.Rect(mouse_pos[0]- SQ_SIZE / 2, mouse_pos[1] - SQ_SIZE / 2, SQ_SIZE, SQ_SIZE))
-            else:
-                drawGameState(screen, gs)
+                drawGameState(screen, gs, sqSelected)
+                screen.blit(IMAGES[selected_piece],
+                            p.Rect(mouse_pos[0] - SQ_SIZE / 2, mouse_pos[1] - SQ_SIZE / 2, SQ_SIZE, SQ_SIZE))
 
         clock.tick(MAX_FPS)
         p.display.flip()
 
+
+# responsible for all graphics with current game state
+def drawGameState(screen, gs, sqSelected=None):
+    drawBoard(screen)  # draw the board
+    drawPieces(screen, gs.board, sqSelected)
+
+
+# draw the pieces using the current game state
+def drawPieces(screen, board, sqSelected=None):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r, c]
+            if piece != '--' and (r, c) != sqSelected:  # space is not empty and not the selected square
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r + c) % 2]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
 """
-
-
 def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))  # set up the screen
     p.display.set_caption("Chess")
@@ -147,6 +179,6 @@ def drawPieces(screen, board):
             if piece != '--':  # space is not empty
                 screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
-
+"""
 if __name__ == "__main__":
     main()
