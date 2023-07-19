cfda324f1fbc41c3f03c29adbdfe78f71057dff9
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-07-19 16:42:34 +0530

    added alpha beta pruning to negamax algo

--- BestMoveFinder.py
+++ BestMoveFinder.py
@@ -1,9 +1,10 @@
 import random
+import numpy as np
 
 pieceScore = {"K": 0, "Q": 9, "R": 5, "N": 3, "B": 3, "p": 1}
 CHECKMATE = 1000
 STALEMATE = 0
-DEPTH = 2
+DEPTH = 4
 nextMove = None  # set a global variable to store the best possible move in current game state
 
 
@@ -11,11 +12,12 @@ def findBestMove(gs, validMoves):
     # helper method to make first recursive call
     global nextMove
     # findMoveMinMax(gs, validMoves, DEPTH, gs.whiteToMove)
-    findMoveNegaMax(gs, validMoves, DEPTH, 1 if gs.whiteToMove else 0)
+    findMoveNegaMax(gs, validMoves, DEPTH, -CHECKMATE, CHECKMATE, 1 if gs.whiteToMove else 0)
     return nextMove
 
 
-def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
+def findMoveNegaMax(gs, validMoves, depth, alpha, beta, turnMultiplier):
+    # NegaMax with Alpha Beta Pruning
     global nextMove
     if depth == 0:
         return turnMultiplier * scoreBoard(gs)
@@ -25,13 +27,17 @@ def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
     for move in validMoves:
         gs.makeMove(move)
         nextMoves = gs.getValidMoves()
-        score = -findMoveNegaMax(gs, nextMoves, depth - 1, -turnMultiplier)
+        score = -findMoveNegaMax(gs, nextMoves, depth - 1, -beta, -alpha, -turnMultiplier)
         if score > maxScore:
             maxScore = score
             if depth == DEPTH:
                 nextMove = move
 
         gs.undoMove()
+        if maxScore > alpha:  # pruning happens
+            alpha = maxScore
+        if alpha >= beta:
+            break
     return maxScore
 
 
@@ -128,3 +134,117 @@ def scoreBoard(gs):
                 score -= pieceScore[square[1]]
 
     return score
+
+
+def evaluate_board(board):
+    # another way of evaluating board
+    piece_values = {'p': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 0}  # Piece values
+    piece_squares = {
+        'p': np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [50, 50, 50, 50, 50, 50, 50, 50],
+            [10, 10, 20, 30, 30, 20, 10, 10],
+            [5, 5, 10, 25, 25, 10, 5, 5],
+            [0, 0, 0, 20, 20, 0, 0, 0],
+            [5, -5, -10, 0, 0, -10, -5, 5],
+            [5, 10, 10, -20, -20, 10, 10, 5],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]),
+        'N': np.array([
+            [-50, -40, -30, -30, -30, -30, -40, -50],
+            [-40, -20, 0, 0, 0, 0, -20, -40],
+            [-30, 0, 10, 15, 15, 10, 0, -30],
+            [-30, 5, 15, 20, 20, 15, 5, -30],
+            [-30, 0, 15, 20, 20, 15, 0, -30],
+            [-30, 5, 10, 15, 15, 10, 5, -30],
+            [-40, -20, 0, 5, 5, 0, -20, -40],
+            [-50, -40, -30, -30, -30, -30, -40, -50]
+        ]),
+        'B': np.array([
+            [-20, -10, -10, -10, -10, -10, -10, -20],
+            [-10, 0, 0, 0, 0, 0, 0, -10],
+            [-10, 0, 5, 10, 10, 5, 0, -10],
+            [-10, 5, 5, 10, 10, 5, 5, -10],
+            [-10, 0, 10, 10, 10, 10, 0, -10],
+            [-10, 10, 10, 10, 10, 10, 10, -10],
+            [-10, 5, 0, 0, 0, 0, 5, -10],
+            [-20, -10, -10, -10, -10, -10, -10, -20]
+        ]),
+        'R': np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [5, 10, 10, 10, 10, 10, 10, 5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [0, 0, 0, 5, 5, 0, 0, 0]
+        ]),
+        'Q': np.array([
+            [-20, -10, -10, -5, -5, -10, -10, -20],
+            [-10, 0, 0, 0, 0, 0, 0, -10],
+            [-10, 0, 5, 5, 5, 5, 0, -10],
+            [-5, 0, 5, 5, 5, 5, 0, -5],
+            [0, 0, 5, 5, 5, 5, 0, -5],
+            [-10, 5, 5, 5, 5, 5, 0, -10],
+            [-10, 0, 5, 0, 0, 0, 0, -10],
+            [-20, -10, -10, -5, -5, -10, -10, -20]
+        ]),
+        'K': np.array([
+            [20, 30, 10, 0, 0, 10, 30, 20],
+            [20, 20, 0, 0, 0, 0, 20, 20],
+            [-10, -20, -20, -20, -20, -20, -20, -10],
+            [-20, -30, -30, -40, -40, -30, -30, -20],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30]
+        ])
+    }
+
+    # Initialize evaluation values
+    material_eval = 0
+    mobility_eval = 0
+    king_safety_eval = 0
+    pawn_structure_eval = 0
+    piece_activity_eval = 0
+
+    # Iterate over the board
+    for row in range(8):
+        for col in range(8):
+            piece = board[row][col]
+
+            # Evaluate material balance
+            if piece[0] == 'w':
+                material_eval += piece_values[piece[1]]
+            elif piece[0] == 'b':
+                material_eval -= piece_values[piece[1]]
+
+            # Evaluate piece-square tables
+            if piece[0] == 'w':
+                piece_activity_eval += piece_squares[piece[1]][row][col]
+            elif piece[0] == 'b':
+                piece_activity_eval -= piece_squares[piece[1]][7 - row][col]
+
+            # Evaluate pawn structure
+            if piece == 'wp':
+                pawn_structure_eval += 10
+            elif piece == 'bp':
+                pawn_structure_eval -= 10
+
+            # Evaluate king safety
+            if piece == 'wK':
+                king_safety_eval += 5
+            elif piece == 'bK':
+                king_safety_eval -= 5
+
+    # Combine the evaluation factors with appropriate weights
+    evaluation = (
+            100 * material_eval +
+            10 * mobility_eval +
+            10 * king_safety_eval +
+            5 * pawn_structure_eval +
+            5 * piece_activity_eval
+    )
+
+    return evaluation
